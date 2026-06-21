49be06d6286502d6d898642c155825e7a95a35b4
ZakiWebsite <24038@burnside.school.nz>
2026-06-22 09:55:40 +1200

    halfway through working on timer

---
+++ timer.js
@@ -0,0 +1,6 @@
+const Days = document.getElementById('days');
+const Hours = document.getElementById('hours');
+const Minutes = document.getElementById('minutes');
+const Seconds = document.getElementById('seconds');
+
+const TargetDate = new Date('')
