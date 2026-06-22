2d953a59b96082e2c1ae15c7cec3b5487b84db07
ZakiWebsite <24038@burnside.school.nz>
2026-06-23 10:22:31 +1200

    added hero section and made timer

--- timer.js
+++ timer.js
@@ -3,4 +3,31 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-const TargetDate = new Date('')
+const targetDate = new Date('July 20 2026 07:00:00').getTime();
+
+function timer () {
+    const currentDate = new Date().getTime();
+    const distance = targetDate - currentDate;
+
+    const days = Math.floor(distance / 1000 / 60 /60 / 24)
+    const hours = Math.floor(distance / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(distance / 1000 / 60) % 60;
+    const seconds = Math.floor(distance / 1000) % 60;
+
+    console.log(days + ':' + hours + ':' + minutes + ':' + seconds);
+
+    Days.innerHTML = days;
+    Hours.innerHTML = hours;
+    Minutes.innerHTML = minutes;
+    Seconds.innerHTML = seconds;
+
+    if(distance < 0) {
+        clearInterval(timer);
+        Days.innerHTML = "00";
+        Hours.innerHTML = "00";
+        Minutes.innerHTML = "00";
+        Seconds.innerHTML = "00";
+    }
+}
+
+setInterval(timer, 1000);
